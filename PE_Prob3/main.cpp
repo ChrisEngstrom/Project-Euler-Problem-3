bc7ff912d0b673ba94878a972926d1c329b6391c
Chris Engstrom <tipher88@gmail.com>
2012-03-12 01:34:00 -0500

    Fixed tabs.

--- PE_Prob3/main.cpp
+++ PE_Prob3/main.cpp
@@ -1,8 +1,8 @@
 //****************************
-// Chris Engstrom			 *
+// Chris Engstrom            *
 // Project Euler - Problem 3 *
 //*******************************************************************
-// What is the largest prime factor of the number 600851475143 ?	*
+// What is the largest prime factor of the number 600851475143 ?    *
 //*******************************************************************
 
 #include <iostream>
@@ -11,108 +11,108 @@
 using namespace std;
 
 // CONSTANTS //
-const bool TIMED_RUN = false;		// Whether or not you want to see how long the program took to run
+const bool TIMED_RUN = false;       // Whether or not you want to see how long the program took to run
 
 // FUNCTION PROTOTYPES //
-bool checkForPrime(__int64 possiblePrime);					// Check to see if the number passed in is a prime number
-template <typename type> type intSqrt (type remainder);		// Find the integer square root of any positive number
+bool checkForPrime(__int64 possiblePrime);                  // Check to see if the number passed in is a prime number
+template <typename type> type intSqrt (type remainder);     // Find the integer square root of any positive number
 
 int main() {
-	// Variables to track how long the program took to run
-	clock_t timeStart,
-			timeEnd;
+    // Variables to track how long the program took to run
+    clock_t timeStart,
+            timeEnd;
 
-	if(TIMED_RUN)
-	{
-		// Get the time when the program starts
-		timeStart = clock();
-	}
+    if(TIMED_RUN)
+    {
+        // Get the time when the program starts
+        timeStart = clock();
+    }
 
-	bool found = false;						// Flag for when the largest prime factor is found
-	__int64 number = 600851475143,			// Number to find the largest prime factor of
-			divisor = intSqrt(number);		// Holds the possible prime factors as the loop is run,
-											//	starts at the square root of the initial number
+    bool found = false;                     // Flag for when the largest prime factor is found
+    __int64 number = 600851475143,          // Number to find the largest prime factor of
+            divisor = intSqrt(number);      // Holds the possible prime factors as the loop is run,
+                                            //	starts at the square root of the initial number
 
-	// Introduce the program
-	cout << "This program finds the largest prime factor of a number." << endl << endl;
+    // Introduce the program
+    cout << "This program finds the largest prime factor of a number." << endl << endl;
 
-	// Starting with the largest possible prime factor, work our way down checking numbers as we go
-	do {
-		// Check to see if the divisor goes into the number without a remainder
-		if(!(number % divisor)) {
-			// Check if the divisor is a prime number, if it is we have found the largest prime factor
-			found = checkForPrime(divisor);
-		}
-		
-		divisor--;		// Decrement the possible prime factor for the next run
+    // Starting with the largest possible prime factor, work our way down checking numbers as we go
+    do {
+        // Check to see if the divisor goes into the number without a remainder
+        if(!(number % divisor)) {
+            // Check if the divisor is a prime number, if it is we have found the largest prime factor
+            found = checkForPrime(divisor);
+        }
+        
+        divisor--;		// Decrement the possible prime factor for the next run
 
-	} while ( divisor > 1 && !found );		// Continue as long as the divisor is greater than one,
-											//	and we haven't found the largest prime factor
+    } while ( divisor > 1 && !found );      // Continue as long as the divisor is greater than one,
+                                            //  and we haven't found the largest prime factor
 
-	// Report the results
-	cout << "The largest prime factor of " << number << " is: " << divisor + 1 << endl;
+    // Report the results
+    cout << "The largest prime factor of " << number << " is: " << divisor + 1 << endl;
 
-	if(TIMED_RUN)
-	{
-		// Get the time when the program is done
-		timeEnd = clock();
+    if(TIMED_RUN)
+    {
+        // Get the time when the program is done
+        timeEnd = clock();
 
-		// Tell the user how long the program took to finish it's job
-		cout << endl << "The program took " << ((double)timeEnd - (double)timeStart) / CLOCKS_PER_SEC << " seconds to run" << endl << endl;
-	}
+        // Tell the user how long the program took to finish it's job
+        cout << endl << "The program took " << ((double)timeEnd - (double)timeStart) / CLOCKS_PER_SEC << " seconds to run" << endl << endl;
+    }
 
-	return 0;
+    return 0;
 }
 
 //*******************************************************************************
-// FUNCTION: bool checkForPrime(int possiblePrime)								*
-//	int possiblePrime - Number that will be checked to see if it is prime		*
-//																				*
-// RETURNS - Whether or not the number passed in is prime						*
+// FUNCTION: bool checkForPrime(int possiblePrime)                              *
+//  int possiblePrime - Number that will be checked to see if it is prime       *
+//                                                                              *
+// RETURNS - Whether or not the number passed in is prime                       *
 //*******************************************************************************
 bool checkForPrime(__int64 possiblePrime)
 {
-	bool isPrime = true;
+    bool isPrime = true;
 
-	// Search for a number that goes into the possiblePrime without a remainder
-	for (int i = 2; i < possiblePrime; i++) {
-		if(!(possiblePrime % i)) {
-			// If such a number is found, the possiblePrime is not a prime number
-			isPrime = false;
-		}
-	}
+    // Search for a number that goes into the possiblePrime without a remainder
+    for (int i = 2; i < possiblePrime; i++) {
+        if(!(possiblePrime % i)) {
+            // If such a number is found, the possiblePrime is not a prime number
+            isPrime = false;
+        }
+    }
 
-	return isPrime;
+    return isPrime;
 }
 
 //***********************************************************************************
-// TEMPLATE: type intSqrt (type remainder)											*
-//	type remainder - number to find the square root of								*
+// TEMPLATE: type intSqrt (type remainder)                                          *
+//  type remainder - number to find the square root of                              *
 //																					*
-// RETURNS - the integer square root of a positive number of any type				*
-//																					*
-// Found on http://www.codecodex.com/wiki/Calculate_an_integer_square_root#C.2B.2B	*
+// RETURNS - the integer square root of a positive number of any type               *
+//                                                                                  *
+// Found on http://www.codecodex.com/wiki/Calculate_an_integer_square_root#C.2B.2B  *
 //***********************************************************************************
 template <typename type>
 type intSqrt (type remainder)  
 {  
-	if (remainder < 0) // if type is unsigned this will be ignored = no runtime  
-		return 0; // negative number ERROR  
+    if (remainder < 0) // if type is unsigned this will be ignored = no runtime  
+        return 0; // negative number ERROR  
   
-	type place = (type)1 << (sizeof (type) * 8 - 2); // calculated by precompiler = same runtime as: place = 0x40000000  
-	while (place > remainder)  
-		place /= 4; // optimized by complier as place >>= 2  
+    type place = (type)1 << (sizeof (type) * 8 - 2); // calculated by precompiler = same runtime as: place = 0x40000000  
+    while (place > remainder)  
+        place /= 4; // optimized by complier as place >>= 2  
   
-	type root = 0;  
-	while (place)  
-	{  
-		if (remainder >= root+place)  
-		{  
-			remainder -= root+place;  
-			root += place * 2;  
-		}  
-		root /= 2;  
-		place /= 4;  
-	}  
+    type root = 0;  
+    while (place)  
+    {  
+        if (remainder >= root+place)  
+        {  
+            remainder -= root+place;  
+            root += place * 2;  
+        }  
+        root /= 2;  
+        place /= 4;  
+    }  
   return root;  
 }  
